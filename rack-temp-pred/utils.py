163688a9c5b9e1f8870cb1de8826b57d54723476
Douglas Jr <dele18037@uvg.edu.gt>
2022-10-10 17:09:54 -0600

    Moved train_case to utils

--- rack-temp-pred/utils.py
+++ rack-temp-pred/utils.py
@@ -2,15 +2,26 @@
 Portions of this page are reproduced from work created and shared by Google and used
 according to terms described in the Creative Commons 4.0 Attribution License.
 """
+from math import sqrt
+
 import pandas as pd
+import tensorflow as tf
 from scipy.signal import savgol_filter
+
+from window import WindowGenerator
 
 
 
 def plot_losses(history):
     """ Plot loss and RMSE with given history. """
 
+    history.history['loss'] = list(map(inverse_transform_loss, history.history['loss']))
+    history.history['val_loss'] = list(map(inverse_transform_loss, history.history['val_loss']))
+    history.history['root_mean_squared_error'] = list(map(inverse_transform_rmse, history.history['root_mean_squared_error']))
+    history.history['val_root_mean_squared_error'] = list(map(inverse_transform_rmse, history.history['val_root_mean_squared_error']))
+
     losses = pd.DataFrame(history.history)
+
 
     losses[['loss', 'val_loss']].plot(figsize=(10,8), xlabel="Epoch", ylabel="Loss", title="Model loss")
     losses[['root_mean_squared_error', 'val_root_mean_squared_error']].plot(figsize=(10,8), xlabel="Epoch", ylabel="RMSE", title="RMSE")
@@ -52,7 +63,42 @@ def normalize_datasets(dataset:pd.DataFrame, outlet_temp_cols:list[str], inlet_t
     dataset[outlet_temp_cols] = (dataset[outlet_temp_cols] - min_temp) / (max_temp - min_temp)
 
     # apply Savitsky-Golay filter
-    dataset[outlet_temp_cols]  = savgol_filter(dataset[outlet_temp_cols], window_length=11, polyorder=3, mode="nearest", axis=0)
-    dataset[inlet_temp_cols]  = savgol_filter(dataset[inlet_temp_cols], window_length=11, polyorder=3, mode="nearest", axis=0)
+    dataset[outlet_temp_cols] = savgol_filter(dataset[outlet_temp_cols], window_length=7, polyorder=3, mode="nearest", axis=0)
+    dataset[inlet_temp_cols] = savgol_filter(dataset[inlet_temp_cols], window_length=7, polyorder=3, mode="nearest", axis=0)
 
     return dataset 
+
+def inverse_transform_loss(x, _min=9.08, _max=33.12):
+    """ Inverse transform loss Min-Max Scaler. """
+
+    return (x * (_max - _min) + _min)
+
+def inverse_transform_rmse(x, _min=9.08, _max=33.12):
+    """ Inverse transform RMSE Min-Max Scaler. """
+
+    return sqrt((x**2) * (_max - _min) + _min)
+
+def train_case(case_df, model, label_cols, in_width, out_steps, max_epochs):
+    """ Train model for given case df. Currently uses 20% of data for validation and 80% for training. """
+    
+    train_df, test_df, val_df = train_test_val_split(case_df, test_size=0, val_size=0.2)
+
+    window = WindowGenerator(
+        train_df=train_df,
+        val_df=val_df,
+        test_df=test_df,
+        input_width=in_width,
+        label_width=out_steps,
+        shift=out_steps,
+        label_columns=label_cols
+    )
+
+    history = model.fit(
+        window.train,
+        epochs=max_epochs,
+        validation_data=window.val,
+        verbose=0
+        # callbacks=[early_stopping]
+    )
+
+    return window, history
